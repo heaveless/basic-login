873ae2a4ef6c213d53de41fdacdc0ece955108ca
experimentallife <user@experimentallife.dev>
2022-08-02 21:10:43 -0500

    :bug: fixed cors

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_migrate import Migrate
 from waitress import serve
 
@@ -6,6 +7,7 @@ from src.app import app, db
 from src.config import logging
 
 migrate = Migrate(app, db)
+cors = CORS(app)
 
 if __name__ == '__main__':
     logging.info("Starting server...")
